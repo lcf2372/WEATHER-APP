9956508cd5d8dc89395789990b5f11c2b4a57b18
Abinesh P <c0917578@mylambton.ca>
2024-12-10 18:45:21 -0500

    Server.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Weather App header', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Weather App/i); // Update this line
+    expect(headerElement).toBeInTheDocument();
 });
